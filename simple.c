6ec67d6e7d4f8a388f13507cf8a1dcc5078e67ce
Ferruh Yigit <ferruh.yigit@intel.com>
2016-11-09 12:51:24 +0000

    eal init
    Signed-off-by: Ferruh Yigit <ferruh.yigit@intel.com>

--- simple.c
+++ simple.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
 
+#include <rte_eal.h>
+
 int main(int argc, char *argv[])
 {
-	printf("\n");
+	int ret;
+
+	/*
+	 * EAL: Environment Abstract Layer"
+	 *
+	 * eal gets parameters from cli, returns number of parsed args
+	 *
+	 * cpu_init: fill cpu_info structure
+	 * log_init
+	 * config_init: create memory configuration in shared memory
+	 * pci_init: scan pci bus
+	 * memory_init (hugepages)
+	 * memzone_init: initialize memzone subsystem
+	 * alarm_init: for timer interrupts
+	 * timer_init
+	 * plugin init
+	 * dev_init: initialize and probe virtual devices
+	 * intr_init: create an interrupt handler thread
+	 * lcore_init: create a thread per lcore
+	 * pci_probe: probe all physical devices
+	 */
+	ret = rte_eal_init(argc, argv);
+	if (ret < 0)
+		return -1;
+
+	argc -= ret;
+	argv += ret;
+
+	/* There is no un-init for eal */
+
 	return 0;
 }

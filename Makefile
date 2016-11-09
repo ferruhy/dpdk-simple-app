b0f65210189482a141aeb717456bd9d359b12a72
Ferruh Yigit <ferruh.yigit@intel.com>
2016-11-09 12:17:19 +0000

    Init
    Signed-off-by: Ferruh Yigit <ferruh.yigit@intel.com>

---
+++ Makefile
@@ -0,0 +1,7 @@
+include $(RTE_SDK)/mk/rte.vars.mk
+
+APP := simple
+
+SRCS-y := simple.c
+
+include $(RTE_SDK)/mk/rte.extapp.mk
---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+# DPDK Side
+# exprrt RTE_SDK=/root/dpdk
+# export RTE_TARGET=x86_64-native-linuxapp-gcc
+# make T=$RTE_TARGET O=$RTE_TARGET config
+# make T=$RTE_TARGET O=$RTE_TARGET
+
+export RTE_SDK=/root/dpdk
+export RTE_TARGET=x86_64-native-linuxapp-gcc
